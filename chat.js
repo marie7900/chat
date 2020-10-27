d68f0776f93f310dd95c02cacfa8a3c1f0ec5f23
marie7900 <mmarion790@gmail.com>
2020-10-27 22:54:07 +0100

    Updates

---
+++ chat.js
@@ -0,0 +1,34 @@
+//Make connection
+
+var socket = io.connect('http://localhost:4000/');
+//var socket = io.connect('https://hardcore-mcnulty-65d009.netlify.app/');
+
+var message = document.querySelector('#message');
+var handle = document.querySelector('#handle');
+var btn = document.querySelector('#send');
+var output = document.querySelector('#output');
+var feedback = document.querySelector('#feedback');
+
+//Emit Events
+
+btn.addEventListener('click', function () {
+    socket.emit('chat', {
+        handle: handle.value,
+        message: message.value
+    })
+})
+
+message.addEventListener('keypress', function () {
+    socket.emit('typing', handle.value);
+})
+
+//Listen for events
+
+socket.on('chat', function (data) {
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+})
+
+socket.on('typing', function (data) {
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message... </em></p>';
+})
