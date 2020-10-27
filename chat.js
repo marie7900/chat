e83e68c45d6a35f8696d298483aae778580f31ad
marie7900 <mmarion790@gmail.com>
2020-10-28 00:42:58 +0100

    Updates

--- chat.js
+++ chat.js
@@ -2,7 +2,7 @@
 
 //var socket = io.connect('http://localhost:4000/');
 //var socket = io.connect('https://marie7900.github.io/chat/public/');
-var socket = io();
+var socket = io.connect();
 
 
 var message = document.querySelector('#message');
