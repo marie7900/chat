d302f49a169ae48bd31aaa5409c82491592666a4
marie7900 <mmarion790@gmail.com>
2020-10-28 00:34:25 +0100

    Updates

--- public/chat.js
+++ public/chat.js
@@ -2,8 +2,8 @@
 
 //var socket = io.connect('http://localhost:4000/');
 //var socket = io.connect('https://marie7900.github.io/chat/public/');
-//var socket = io.connect('/');
-var socket = io();
+var socket = io.connect();
+
 
 var message = document.querySelector('#message');
 var handle = document.querySelector('#handle');
